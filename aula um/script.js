891291a3ddd5c6a93dcdbdb2d56762eecde0680f
larinuns <larissa6587@institutoideia.com>
2026-02-26 19:15:57 -0300

    primeiro commit

---
+++ aula um/script.js
@@ -0,0 +1,6 @@
+function mostrarMensagem() { 
+  alert("JavaScript está funcionando!"); 
+}
+function mostrarMensagem() { 
+document.getElementById("texto").innerText = "O texto foi alterado com JavaScript!"; 
+} 
